c8ae71fd673bac6577a7c8c057ce41b6da0a2a93
sarony <saron.yitbarek@gmail.com>
2021-08-24 02:23:50 -0400

    WIP - jQuery installed, trying to get jQuery in todos.js to be recognized

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -7,6 +7,8 @@ require("@rails/ujs").start()
 require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
+require("jquery")
+require("packs/todos")
 
 import "tailwindcss/tailwind.css"
 import "stylesheets/application.scss"
@@ -18,3 +20,5 @@ import "stylesheets/application.scss"
 //
 // const images = require.context('../images', true)
 // const imagePath = (name) => images(name, true)
+
+
---
+++ app/javascript/packs/todos.js
@@ -0,0 +1,5 @@
+window.jQuery = $;
+window.$ = $;
+
+console.log("file loaded 2");
+$(".js-check-off").change(function(){if(this.checked){alert("hi")}});
